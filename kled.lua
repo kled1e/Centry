120c373a25e5a46c767ab8d47b13b4f5c86d347c
kled1e <kleedzzy@gmail.com>
2025-03-17 01:36:37 +0100

    kled.lua

--- kled.lua
+++ kled.lua
@@ -1,3 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kled1e/Centry/refs/heads/main/lib.lua"))()
 
 
+local Window = library:CreateWindow("Centry Hub", "Beta", 10044538000)
+
+
+local Tab = Window:CreateTab("Scripts")
+
+local Page = Tab:CreateFrame("Main")
